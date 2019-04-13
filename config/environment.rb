387d88fcd1e318698098b6f8d8add9744abe4b6f
Carlton Dwayne <carltondgrandison@gmail.com>
2019-04-13 20:00:55 +0000

    changes

---
+++ config/environment.rb
@@ -0,0 +1,6 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri
+
+require_relative '../lib/worlds_best_restaurants/cli'
+require_relative '../lib/worlds_best_restaurants/version'
---
+++ lib/Sixers_Roster/cli.rb
@@ -0,0 +1,9 @@
+#Our CLI Controller
+
+class SixersRoster::CLI
+
+  def call
+    puts "Here is the 76ers roster"
+  end
+
+end
